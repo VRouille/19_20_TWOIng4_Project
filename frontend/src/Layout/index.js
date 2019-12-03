8d20e84e68c7f23059f82dc79415a9030608395f
VRouille <vincent.rouille@edu.ece.fr>
2019-12-03 15:33:07 +0100

    1st commit
    header + sidebar

---
+++ frontend/src/Layout/index.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import clsx from 'clsx';
+import person from './person.png';
+import {
+  makeStyles, useTheme, Drawer, AppBar, Toolbar,
+  List, CssBaseline, Typography, Divider, IconButton,
+  ListItem, ListItemIcon, ListItemText,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
+import StorageRoundedIcon from '@material-ui/icons/StorageRounded';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    root:{
+      display :'flex',
+    },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    backgroundColor: '#ffffff',
+    color:'#000000',
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+    color :'#2C75FF',
+  },
+  hide: {
+    display: 'none',
+    color:'#ffffff',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    backgroundColor: '#2C75FF',
+    fontFamily: 'montserrat',
+    color:'#ffffff',
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+    backgroundColor: '#2C75FF',
+    fontFamily:'montserrat',
+    color:'#ffffff',
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar, 
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  profil:{
+    fontFamily :'montserrat',
+    fontWeight : '600px',
+    paddingLeft : '70%',
+  },
+  person:{
+    height:'40px',
+    width: '40px',
+    borderRadius: '50%',
+  },
+  ColorIcon: {
+    color:'#ffffff',
+    paddingLeft: theme.spacing(1),
+    fontFamily: 'montserrat',
+  },
+
+}));
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+          })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.profil} variant="h6" noWrap>
+            Bonjour Clément
+          </Typography>
+          <img className={classes.person} src={person} alt="person"></img>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <List>
+          {['Dashboard', 'Formulaire'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon className={classes.ColorIcon}>{index % 2 === 0 ? <DashboardRoundedIcon /> : <StorageRoundedIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+}
